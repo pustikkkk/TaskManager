e1bc9ac608f7b22bc45c7a507ea4ea441806b468
palexey2006 <palexey2006@gmail.com>
2026-04-26 16:59:09 -0400

    worked all around the project but the main thing is that I created a new migration which will drop default auth name column in users table and then will add username column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,22 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <header class="bg-blue-700">
-        <a class="bg-red-400">Log out</a>
-        <a class="bg-red-400">Create a Task</a>
-    </header>
-    <main>
-        @yield('content')
-    </main>
-
+<header>
+    <nav class="h-12 bg-black flex items-center px-6">
+        <a href="{{route('dashboard')}}" class="text-lg text-white">Task Manager</a>
+        @if (request()->routeIs('dashboard'))
+            <div class="ml-auto flex gap-4">
+                <a class="text-lg text-white">Create a task</a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button class="text-lg text-red-200 bg-transparent">Log out</button>
+                </form>
+            </div>
+        @endif
+    </nav>
+</header>
+<main>
+    @yield('content')
+</main>
 </body>
 </html>
