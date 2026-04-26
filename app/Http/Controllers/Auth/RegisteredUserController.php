f74976ad0ef2539004053957342f3b71032868c6
palexey2006 <palexey2006@gmail.com>
2026-04-26 17:06:45 -0400

    Have dropped name column and instead created username column, after that I will implement email verification for users

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,7 +31,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:20', 'unique:users'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
--- database/migrations/2026_04_26_205757_update_users_table_for_username.php
+++ database/migrations/2026_04_26_205757_update_users_table_for_username.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->string('username')->unique()->after('id');
+            $table->dropColumn('name');
         });
     }
 
