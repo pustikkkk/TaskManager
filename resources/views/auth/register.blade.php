60279392e051b5c347f3e8c9cdc2d1c9e036ccbc
palexey2006 <palexey2006@gmail.com>
2026-04-25 21:16:01 -0400

    finished registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,37 @@
-<?php
+@extends('layouts.guest')
+
+@section('title', 'Login')
+@section('content')
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('register') }}" method="POST" class="mt-52 border-2  border-black">
+        @csrf
+        <div class="m-10 text-center">
+            <label for="name">Name</label>
+            <input id="name" type="text" name="name" class="ml-10">
+        </div>
+        <div class="m-10 text-center">
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" class="ml-10">
+        </div>
+        <div class="m-10 text-center">
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" class="ml-10">
+        </div>
+        <div class="m-10 text-center">
+            <label for="password_confirmation">Confirm</label>
+            <input id="password_confirmation" type="password" name="password_confirmation" class="ml-10">
+        </div>
+        <div class="m-10 text-center">
+            <button type="submit" class="bg-green-950 text-white p-2">Register</button>
+        </div>
+
+    </form>
+@endsection
