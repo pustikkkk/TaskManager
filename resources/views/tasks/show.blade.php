98844ad3b1c9c87aa97291691f2d12b46d147cba
palexey2006 <palexey2006@gmail.com>
2026-05-03 14:13:37 -0400

    added content to a show.blade.php

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,1 +1,13 @@
-<?php
+@php use Carbon\Carbon; @endphp
+@extends('layouts.app')
+@section('title', 'Show task')
+@section('content')
+    <div>
+        <h1>{{$task->title}}</h1>
+        <p><strong>Status:</strong> {{$task->status}}</p>
+        <p><strong>Priority:</strong> {{$task->priority}}</p>
+
+        <p id="description">Description:{{$task->description}}</p>
+        <p>Due: {{Carbon::parse($task->due_date)->format('M d, Y')}}</p>
+    </div>
+@endsection
