1dbdeca9235e6a6a292d4f158529ef76e53a522c
palexey2006 <palexey2006@gmail.com>
2026-05-03 12:01:03 -0400

    have implemented basic logic for a task dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,1 +1,12 @@
 @extends('layouts.app')
+@section('title', 'Dashboard')
+@section('content')
+    <h1 class="text-4xl font-bold m-4 text-center">Task Dashboard</h1>
+    <ul>
+        @forelse($tasks as $task)
+            <li>{{ $task->title }}</li>
+        @empty
+            <li class="text-center">No Task Found</li>
+        @endforelse
+    </ul>
+@endsection
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,6 +2,8 @@
 @section('title', 'Create a task')
 @section('content')
     <form action='{{route('tasks.store')}}' method='POST'>
+        <div>
 
+        </div>
     </form>
 @endsection
