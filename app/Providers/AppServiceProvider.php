79e824adac2d7a7fec8038f44fb61f42ed78a91f
palexey2006 <palexey2006@gmail.com>
2026-05-14 14:01:29 -0400

    Switched email verif link from localhost to my own domain

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
-            URL::forceScheme('https');
+            URL::forceRootUrl(config('app.url'));
         }
 
         Mail::extend('sendgrid', function () {
