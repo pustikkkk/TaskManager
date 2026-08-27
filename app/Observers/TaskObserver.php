4e8f46989c808d250f393186dec41bb7cf4067c5
palexey2006 <palexey2006@gmail.com>
2026-08-27 15:21:02 -0400

    Fix config key mismatch in TaskObserver

--- app/Observers/TaskObserver.php
+++ app/Observers/TaskObserver.php
@@ -13,8 +13,8 @@ class TaskObserver
      */
     public function created(Task $task): void
     {
-        Log::info('OBSERVER FIRED', ['task' => $task->id]);
-        $url = config('services.make.url');
+Log::info('OBSERVER FIRED', ['task' => $task->id]);
+        $url = config('services.make_webhook_url');
         if(!$url) {
             return;
         }
